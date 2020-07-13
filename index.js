6c186128c8db6e19689340858014df98322d3e75
Necro <swordigo7@gmail.com>
2020-07-13 16:19:30 +0300

    Работа с карточками

--- index.js
+++ index.js
@@ -32,27 +32,29 @@ var x = Vue.component('card_assembl',
         <br>
         <p class="name-card">{{name}}</p>
         <br>
-        <p>{{text}}</p>
-        <table border="0" cellspacing="10pt">
+        <p class="text-card">{{text}}</p>
+        <img src="perc1.jpg">
+        <div style="padding-left:20px;"><table border="0" cellspacing="10pt">
             <tr>
                 <td>
                     <p style="font-family: Rubik; font-size: 10px; line-height: 14px; color:#7D7D7D">необходимо собрать</p>
-                </td>
-                <td>
+                </td><td>
                     <p style="font-family: Rubik; font-size: 10px; line-height: 14px; color:#7D7D7D">конец</p>
                 </td>
             </tr>
             <tr>
                 <td>
-                        <b style="font-family: Rubik; font-size: 12px; font-weight: bold; line-height: 20px; color:#8DCA78">45 194</b>
-                        <b style="font-family: Rubik; font-size: 12px; font-weight: bold; line-height: 20px; color:#7D7D7D">из 1 000 000 RUB</b>
-                </td>
-                <td>
-                    <b style="font-family: Rubik; font-size: 12px; line-height: 20px; color:#7D7D7D">завтра</b>
+                    <b style="font-family: Rubik; font-size: 12px; font-weight: bold; line-height: 20px; color:#8DCA78">45 194</b>
+                    <b style="font-family: Rubik; font-size: 12px; font-weight: bold; line-height: 20px; color:#7D7D7D">из 1 000 000 RUB</b>
+                </td><td>
+                    <b style="padding-right:20px; font-family: Rubik; font-size: 12px; line-height: 20px; color:#7D7D7D">завтра</b>
                 </td>
             </tr>
-        </table>
-        <div>
+        </table></div>
+                        
+                        
+                    
+        <div style="padding-left: 20px;">
             <button disabled class="tagq">помочь нуждающимся</button>
             <button disabled class="tagq">закят</button>
         </div>
