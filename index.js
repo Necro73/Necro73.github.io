45c6caebefd0674f35617f54c7d74cea097e73bc
Necro <swordigo7@gmail.com>
2020-07-14 14:07:17 +0300

    Карточки работа с процентной диаграммой

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ var x = Vue.component('card_assembl',
         {
             type: String,
         },
+        imageq2:
+        {
+            type: String,
+        },
         name:
         {
             type: String,
@@ -23,6 +27,7 @@ var x = Vue.component('card_assembl',
     {
         return{
             imgs: this.imageq,
+            imgs2: this.imageq2,
         }
     },
     template:
@@ -33,28 +38,29 @@ var x = Vue.component('card_assembl',
         <p class="name-card">{{name}}</p>
         <br>
         <p class="text-card">{{text}}</p>
-        <img src="perc1.jpg" class="img2">
-        <div style="padding-left:20px;"><table border="0" cellspacing="10pt">
-            <tr>
-                <td>
-                    <p style="font-family: Rubik; font-size: 10px; line-height: 14px; color:#7D7D7D">необходимо собрать</p>
-                </td><td>
-                    <p style="font-family: Rubik; font-size: 10px; line-height: 14px; color:#7D7D7D">конец</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <b style="font-family: Rubik; font-size: 12px; font-weight: bold; line-height: 20px; color:#8DCA78">45 194</b>
-                    <b style="font-family: Rubik; font-size: 12px; font-weight: bold; line-height: 20px; color:#7D7D7D">из 1 000 000 RUB</b>
-                </td><td>
-                    <b style="padding-right:20px; font-family: Rubik; font-size: 12px; line-height: 20px; color:#7D7D7D">завтра</b>
-                </td>
-            </tr>
-        </table></div>
-                        
+        <img :src="imgs2" class="img2" />
+        <div style="display:flex; width:100%; height:15px; padding-left:20px; padding-right:20px;">
+            <div style="padding-right:20px">
+                <p style="padding-left:20px; padding-right:20px; width:175px; font-family: Rubik; font-size: 10px; line-height: 14px; color:#7D7D7D">необходимо собрать</p>
+            </div>
+            <div>
+                <p style="width:77px; font-family: Rubik; font-size: 10px; line-height: 14px; color:#7D7D7D">конец</p>
+            </div>
+        </div>
+        <div style="display:flex; width:100%; height:15px; padding-left:20px; padding-right:20px;">
+            <div>
+                <b style="padding-left:20px; font-family: Rubik; font-size: 12px; font-weight: bold; line-height: 20px; color:#8DCA78">45 194</b>
+            </div>
+            <div style="padding-left:5px;" style="padding-right:17px">
+                <b style="padding-right:20px; font-family:Rubik; font-size:12px; font-weight:bold; line-height:20px; color:#7D7D7D">из 1 000 000 RUB</b>
+            </div>
+            <div>
+                <b style="font-family: Rubik; font-size: 12px; line-height: 20px; color:#7D7D7D">завтра</b>
+            </div>
+        </div>         
                         
                     
-        <div style="padding-left: 20px;">
+        <div style="padding-left: 20px; padding-top: 22px">
             <button disabled class="tagq">помочь нуждающимся</button>
             <button disabled class="tagq">закят</button>
         </div>
